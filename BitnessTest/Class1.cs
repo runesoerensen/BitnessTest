068fe1022c665ef64c227ee8a41fff41e73e7f8a
Rune Soerensen <runesoerensen@gmail.com>
2014-09-05 13:25:47 -0700

    Initialize SvnRepositoryClient
    This should cause test/runtime failures due to the architecture mismatch

--- BitnessTest/Class1.cs
+++ BitnessTest/Class1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SharpSvn;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ namespace BitnessTest
         [Fact]
         public void Foo()
         {
+            var client = new SvnRepositoryClient();
             Assert.Equal(1, 1);
         }
     }
