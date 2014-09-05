09b194d540dbb55343a71b3bb9db0527d7b69411
Rune Soerensen <runesoerensen@gmail.com>
2014-09-05 13:23:09 -0700

    Add foo test

--- BitnessTest/Class1.cs
+++ BitnessTest/Class1.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace BitnessTest
 {
     public class Class1
     {
+        [Fact]
+        public void Foo()
+        {
+            Assert.Equal(1, 1);
+        }
     }
 }
